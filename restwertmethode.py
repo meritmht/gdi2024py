87a9be3f8eb9be74b86fadf24a519b8d0de5d79c
JoePie-HSWIS <117598158+JoePie-HSWIS@users.noreply.github.com>
2024-05-13 09:13:18 +0000

    Mon, May 13, 2024, 9:13 AM +00:00

--- restwertmethode.py
+++ restwertmethode.py
@@ -3,4 +3,5 @@ def convert_to_base(num, base):
         raise ValueError("Base must be between 2 and 16.")
     
     while num > 0:
-         
+        num = num // base # Quotient der ganzzahligen Division
+        remainder = num % base # Rest der ganzzahligen Division
