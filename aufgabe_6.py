79aa02352364c0ebb04f4c8104f41e7d27c806f0
JoePie-HSWIS <117598158+JoePie-HSWIS@users.noreply.github.com>
2024-05-13 13:36:09 +0000

    Mon, May 13, 2024, 1:36 PM +00:00

--- aufgabe_6.py
+++ aufgabe_6.py
@@ -1,8 +1,8 @@
 import math
 
-alphabet = ['a', 'e', 'i', 'o', 'u']
+alphabet      = ['a' , 'e', 'i', 'o',  'u']
 probabilities = [0.25, 0.2, 0.1, 0.3, 0.15]
-lengths = [2,2,3,2,3]
+lengths       = [2   , 2  ,   3,   2,    3]
 
 L = sum(p * l for p, l in zip(probabilities, lengths))
 
