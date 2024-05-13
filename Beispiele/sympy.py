8996d0e56b6b6bfc1d871150f69dbef7803a42a3
JoePie-HSWIS <117598158+JoePie-HSWIS@users.noreply.github.com>
2024-05-13 13:57:24 +0000

    Mon, May 13, 2024, 1:57 PM +00:00

--- Beispiele/sympy.py
+++
@@ -1,9 +0,0 @@
-# AUFGABE 4.3
-
-from sympy.logic.boolalg import And, Or, Not, Implies, Equivalent
-from sympy.logic.boolalg import to_cnf
-from sympy.abc import A, B, C
-
-f = ( ((A & ~B) >> C) & ( C >> (A | B) ) & ( ~( A & C) ))
-f
-
